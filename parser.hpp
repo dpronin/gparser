c9101547341b7589e8a8b6fd56a64a0088b6bb33
Denis Pronin <dannftk@yandex.ru>
2023-03-03 23:07:47 +0300

    parse is upgraded, now it is possible to give any output iterator type

--- parser.hpp
+++ parser.hpp
@@ -1,6 +1,9 @@
 #include <cctype>
+#include <cstddef>
 #include <cstdint>
+
 #include <exception>
+#include <iterator>
 #include <limits>
 #include <stack>
 #include <stdexcept>
@@ -10,24 +13,28 @@
 namespace gtask {
 
 struct params {
-    std::string::size_type out_max_len = std::numeric_limits<std::string::size_type>::max();
+    size_t out_max_len = std::numeric_limits<size_t>::max();
 };
 
-template <typename Iterator>
-std::string parse(Iterator first, Iterator last, params const &params = {})
+template <typename InputIt, typename OutputIt>
+OutputIt parse(InputIt first, InputIt last, OutputIt result, params const &params = {})
 {
-    std::string result;
     std::stack<std::pair<std::string, uint32_t>> s;
 
-    std::string::size_type result_estim_len = std::string::size_type{0};
+    size_t result_estim_len{0};
 
-    for (std::string *p = &result; first != last; ++first) {
+    for (; first != last; ++first) {
         switch (auto const c = *first) {
         case 'a'...'z':
         case 'A'...'Z':
             if (++result_estim_len > params.out_max_len)
                 throw std::range_error(std::string{"result string exceeds max len "} + std::to_string(params.out_max_len) + " given in params");
-            p->push_back(c);
+
+            if (s.empty())
+                *result++ = c;
+            else
+                s.top().first.push_back(c);
+
             break;
         case '1'...'9': {
             uint64_t k = 0;
@@ -44,7 +51,6 @@ std::string parse(Iterator first, Iterator last, params const &params = {})
                 throw std::invalid_argument(std::string{"unexpected character '"} + *first + "', expected '['");
 
             s.push({{}, static_cast<uint32_t>(k)});
-            p = &s.top().first;
 
             break;
         }
@@ -57,16 +63,20 @@ std::string parse(Iterator first, Iterator last, params const &params = {})
             if (r.empty())
                 throw std::invalid_argument("empty string parsed inside the expression []");
 
-            s.pop();
-            p = s.empty() ? &result : &s.top().first;
-
             result_estim_len += (k - 1) * r.size();
             if (result_estim_len > params.out_max_len)
                 throw std::range_error(std::string{"result string exceeds max len "} + std::to_string(params.out_max_len) + " given in params");
 
-            p->reserve(p->size() + k * r.size());
-            for (auto n = k; n--;)
-                *p += r;
+            s.pop();
+            if (s.empty()) {
+                for (auto n = k; n--;)
+                    result = std::copy(r.cbegin(), r.cend(), result);
+            } else {
+                auto &expr = s.top().first;
+                expr.reserve(expr.size() + k * r.size());
+                for (auto n = k; n--;)
+                    expr += r;
+            }
         } break;
         default:
             throw std::invalid_argument(std::string{"unexpected character '"} + c + "'");
@@ -81,7 +91,10 @@ std::string parse(Iterator first, Iterator last, params const &params = {})
 
 inline std::string parse(std::string_view input, params const &params = {})
 {
-    return parse(input.cbegin(), input.cend(), params);
+    std::string result;
+    result.reserve(std::min((input.size() + 1) / 2, params.out_max_len));
+    parse(input.cbegin(), input.cend(), std::back_inserter(result), params);
+    return result;
 }
 
 } // namespace gtask
